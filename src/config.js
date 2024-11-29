5fa079a922ea0ed18a7f66ad5b4d4652060f9c1c
Stephen Augustus <foo@auggie.dev>
2024-11-29 03:48:15 -0500

    src/config.js: Update site title and description
    Signed-off-by: Stephen Augustus <foo@auggie.dev>

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SITE_TITLE = "Cassidy's blog template";
-export const SITE_DESCRIPTION = "A blog template for you!";
+export const SITE_TITLE = "blahg[-bsky]";
+export const SITE_DESCRIPTION = "...and it's the same blog template, but there are bluesky comments, so it's not";
